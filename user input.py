daaef0abce7a2374b53bb3b990f355e9100a4b56
Anuragsharma1311 <124031761+Anuragsharma1311@users.noreply.github.com>
2023-02-03 16:50:56 +0530

    Add files via upload

---
+++ user input.py
@@ -0,0 +1,21 @@
+a=int(input('Enter a no.'))
+print(a)
+b=int(input('Enter a no.'))
+print(b)
+print(a+b)
+
+
+c=float(input('Enter a no'))
+print(c)
+d=float(input('Enter a no'))
+print(d)
+print(c+d)
+
+e=eval(input('Enter a no'))
+print(e)
+f=eval(input('Enter a no'))
+print(f)
+print(e+f)
+
+
+
